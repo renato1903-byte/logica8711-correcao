778e9b0d50685402273ca822c657beaffbe10097
RENATO CHASSOT COLOMBO <03917718090@senacrs.edu.br>
2026-03-27 19:42:16 -0300

    int,float e double

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+int main(){
+
+   int a = 8;
+   //int número inteiro
+   float b = 9.6;
+   //float número de ponto flutuante ocupa 4bytes.
+   double c = 4.6;
+   //double ocupa 8bytes.
+
+
+   std::cout<<a<<std::endl;
+   std::cout<<b<<std::endl;
+   std::cout<<c<<std::endl;
+
+    return 0;
+}
