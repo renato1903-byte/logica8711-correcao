6626883db8cbd39743cd3e7d4823df02f14497c8
RENATO CHASSOT COLOMBO <03917718090@senacrs.edu.br>
2026-03-27 20:07:01 -0300

    double

--- main.cpp
+++ main.cpp
@@ -1,6 +1,12 @@
 #include<iostream>
+#include<iomanip>
 
 int main(){
+
+    double d = 8.6952231456;
+
+       std::cout<<std::fixed<<std::setprecision(10)<<d<<std::endl;
+
 
    int a = 8;
    //int número inteiro
@@ -8,7 +14,6 @@ int main(){
    //float número de ponto flutuante ocupa 4bytes.
    double c = 4.6;
    //double ocupa 8bytes.
-
 
    std::cout<<a<<std::endl;
    std::cout<<b<<std::endl;
