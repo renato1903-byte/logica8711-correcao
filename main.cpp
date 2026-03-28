10cf88fc4306a8b4f7c32a0f2687fc054654b835
RENATO CHASSOT COLOMBO <03917718090@senacrs.edu.br>
2026-03-27 21:50:02 -0300

    idade e altura

--- main.cpp
+++ main.cpp
@@ -3,14 +3,22 @@
 
 int main(){
 
-    int idade = 18;
-    float altura = 1.85;
-    std::string nome = "Renato";
+    std::string nome;
+    int idade;
+    float altura;
 
-    std::cout<<"Meu nome é "<<nome<<"."<<std::endl;
-    std::cout<<"Minhaaltura é "<<altura<<"m."<<std::endl;
-    std::cout<<"Minha idade é "<<idade<<" anos"<<std::endl;
+    std::cout<<"Qual seu nome: "<<std::endl;
+    std::cin>>nome;
+    std::cout<<"Qual a sua idade "<<std::endl;
+    std::cin>>idade;
+    std::cout<<"Qual sua altura "<<std::endl; 
+    std::cin>>altura;
 
+
+    std::cout<<"Bem-vindo(a) "<<nome<<"!!"<<std::endl;
+    std::cout<<"Idade de "<<idade<<"!!"<<std::endl;
+    std::cout<<"Altura de "<<altura<<"!!"<<std::endl;
+    
 
 
     return 0;
