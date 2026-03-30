b081febb549a3b44a749c60a7dda1064e31617b7
RENATO CHASSOT COLOMBO <03917718090@senacrs.edu.br>
2026-03-30 19:53:24 -0300

    dia da semana

--- main.cpp
+++ main.cpp
@@ -1,23 +1,16 @@
 #include<iostream>
-#include<iomanip>
+#include<string>
 
 int main(){
 
-    std::string nome;
-    int idade;
-    float altura;
+   std::string diadasemana;
 
-    std::cout<<"Qual seu nome: "<<std::endl;
-    std::cin>>nome;
-    std::cout<<"Qual a sua idade "<<std::endl;
-    std::cin>>idade;
-    std::cout<<"Qual sua altura "<<std::endl; 
-    std::cin>>altura;
+   std::cout<<"Que dia hoje: "<<std::endl;
+   std::cin>>diadasemana;
 
+   std::cout<<"Hoje é "<<diadasemana<<"!!"<<std::endl;
+   
 
-    std::cout<<"Bem-vindo(a) "<<nome<<"!!"<<std::endl;
-    std::cout<<"Idade de "<<idade<<"!!"<<std::endl;
-    std::cout<<"Altura de "<<altura<<"!!"<<std::endl;
     
 
 
