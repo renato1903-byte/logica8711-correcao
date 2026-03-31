0969a1aae95915a95412db5980c50d07179b341c
RENATO CHASSOT COLOMBO <03917718090@senacrs.edu.br>
2026-03-30 21:47:12 -0300

    a +=5 (a=10)

--- main.cpp
+++ main.cpp
@@ -2,12 +2,17 @@
 
 int main(){
 
-  int a = 8;
- 
-    std::cout<<++a<<std::endl;
-    
+  int a = 10;
+
+
+    a += 5;
+  //a = a + 5
+
+  std::cout<<a<<std::endl;
+
 
     
 
+    
     return 0;
 }
