496f03fd9e98cc7da1a032cced78bbc515d21fea
RENATO CHASSOT COLOMBO <03917718090@senacrs.edu.br>
2026-04-01 21:55:27 -0300

    string nome e sobrenome

--- main.cpp
+++ main.cpp
@@ -2,24 +2,11 @@
 
 int main(){
 
-int num1, num2, soma;
+  std::string nome = "Renato";
+  std::string sobrenome = "Chassot";
 
-std::cout<<"Digite num 1: "<<"\n";
-std::cin>>num1;
-
-std::cout<<"Digite num 2: "<<"\n";
-std::cin>>num2;
-
-soma = num1 + num2;
-
-std::cout<<"O resultadoda soma é "<<soma<<"\n";
-
-
-
-
-
-
-   
+   std::cout<<nome + " " + sobrenome<<std::endl;
+    
 
     return 0;
 }
