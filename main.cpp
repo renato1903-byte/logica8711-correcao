2acba7226e1c35feac8bae44361bbc05f58baaaf
RENATO CHASSOT COLOMBO <03917718090@senacrs.edu.br>
2026-04-01 21:12:15 -0300

    ordem de operações

--- main.cpp
+++ main.cpp
@@ -1,28 +1,14 @@
 #include<iostream>
 
-
-
 int main(){
 
-  std::string nome;
-  int idade;
-  int anoAtual = 2026;
-  
+int result1 = 4 + 4 * 2;
+int result2 = 4 + 2 / 2;
+int result3 = (4 + 4) * 2;
 
-
-
-  std::cout<<"Qual seu nome: "<<std::endl;
-  std::cin>>nome;
-
-  std::cout<<"Qual sua idade: "<<std::endl;
-  std::cin>>idade;
-
-  std::cout<<"Qual anoAtual: "<<std::endl;
-  std::cin>>anoAtual;
-
-  std::cout<<"Bem-vindo: "<<nome<<std::endl;
-
-  std::cout<<"Você nasceu em: "<<(anoAtual-idade)<<std::endl;
+std::cout<<result1<<std::endl;
+std::cout<<result2<<std::endl;
+std::cout<<result3<<std::endl;
 
 
 
