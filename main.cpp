7c54743db96aa8caa599183e18ba77542ae51dc2
RENATO CHASSOT COLOMBO <03917718090@senacrs.edu.br>
2026-04-06 20:27:40 -0300

    soma na string

--- main.cpp
+++ main.cpp
@@ -3,16 +3,17 @@
 
 int main(){
 
-std::string nome;
-std::string sobrenome = " Chassot";
+  int a = 40;
+  int b =10;
+  std::string c = "40";
+  std::string d = "10";
 
-std::cout<<"Digite seu nome: "<<std::endl;
-std::cin>>nome;
-
-std::cout<<nome.append(sobrenome);
+  std::cout<<a+b<<std::endl;
+  std::cout<<c+d<<std::endl;
+  
 
 
 
   return 0;
-  
+
 }
