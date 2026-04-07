6c9146fd455df1a453232f96f0acdec745669ae8
RENATO CHASSOT COLOMBO <03917718090@senacrs.edu.br>
2026-04-06 21:19:31 -0300

    length e size

--- main.cpp
+++ main.cpp
@@ -3,14 +3,13 @@
 
 int main(){
 
-  int a = 40;
-  int b =10;
-  std::string c = "40";
-  std::string d = "10";
+  std::string nome = "Renato";
 
-  std::cout<<a+b<<std::endl;
-  std::cout<<c+d<<std::endl;
+  std::cout<<"Nome tem: "<<nome.length()<<" letras."<<std::endl;
+  std::cout<<"Nome tem: "<<nome.size()<<" letras."<<std::endl;
+
   
+
 
 
 
