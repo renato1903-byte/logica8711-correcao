ae54c15e087739b26b8ea0babf20bdde2ad8094f
RENATO CHASSOT COLOMBO <03917718090@senacrs.edu.br>
2026-04-08 21:36:15 -0300

    a>b e b>a

--- main.cpp
+++ main.cpp
@@ -3,9 +3,14 @@
 
 int main(){
 
-  std::cout<<std::sqrt(64)<<std::endl;
-  std::cout<<std::round(2.6)<<std::endl;
-  std::cout<<std::log(2)<<std::endl;
+  int a = 5;
+  int b = 2;
+
+  if(a > 2){
+    std::cout<<"A é maior que B."<<std::endl;
+  }else{
+    std::cout<<"B é maior que A."<<std::endl;
+  }
 
 
   
