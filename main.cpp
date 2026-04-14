e1bfd2b9fd3215a3cca52ecd4528ddced285f9b8
RENATO CHASSOT COLOMBO <03917718090@senacrs.edu.br>
2026-04-13 21:13:38 -0300

    formas de pagamento 2.0

--- main.cpp
+++ main.cpp
@@ -9,13 +9,14 @@
     float valorParafuso = 1.40;
     float valorTotalParafuso;
     float desconto;
+    float juros;
 
     std::cout<<"Digite a quantidade de parafusos desejada: "<<std::endl;
     std::cin>>qtdParafuso;
 
     valorTotalParafuso = qtdParafuso = valorParafuso;
 
-    std::cout<<"Qual a for de pagamento: "<<std::endl;
+    std::cout<<"Qual a forma de pagamento: "<<std::endl;
     std::cout<<"--- 1 - Pix"<<std::endl;
     std::cout<<"--- 2 - Dinheiro"<<std::endl;
     std::cout<<"--- 3 - Cartão"<<std::endl;
@@ -27,6 +28,7 @@
       desconto = valorTotalParafuso * 0.90;
       std::cout<<"Sua compra ficou no valor total de R$: "<<desconto<<std::endl;
     }else{
+      juros = valorTotalParafuso;
       std::cout<<"Sua compra ficou no valor total R$: "<<valorTotalParafuso<<std::endl;
     }
 
