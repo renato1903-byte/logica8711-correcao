bf53f94206152ce2301572b0ee864e4028263f93
RENATO CHASSOT COLOMBO <03917718090@senacrs.edu.br>
2026-04-15 20:56:10 -0300

    soma em (for)

--- main.cpp
+++ main.cpp
@@ -2,38 +2,13 @@
 
 int main(){
 
-    int opcao;
+   int soma = 0;
 
-    std::cout<<"Digite um valor para opção: "<<std::endl;
-    std::cin>>opcao;
-
-    switch(opcao){
-      case 1:
-          std::cout<<"Domingo!"<<std::endl;
-          break;
-      case 2:
-          std::cout<<"Segunda-feira!"<<std::endl;
-          break;
-      case 3:
-          std::cout<<"Terça-feira!"<<std::endl;
-          break;
-      case 4:
-          std::cout<<"Quarta-feira!"<<std::endl;
-          break;
-      case 5:
-          std::cout<<"Quinta-feira!"<<std::endl;
-          break;
-      case 6:
-          std::cout<<"Sexta-feira!"<<std::endl;
-          break;
-      case 7:
-          std::cout<<"Sábado!"<<std::endl;
-          break;
-      default:
-          std::cout<<"Número inválido!"<<std::endl;
-    }
-
+   for(int i = 1; i <= 5; i++){
+        soma = soma + i;
+   }
   
+      std::cout<<"Soma é: "<<soma<<std::endl;
 
 
   return 0;
