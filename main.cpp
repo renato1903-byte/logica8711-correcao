fc650d12402ad44c79081af0925e6f07af59597d
RENATO CHASSOT COLOMBO <03917718090@senacrs.edu.br>
2026-04-15 19:45:08 -0300

    programa

--- main.cpp
+++ main.cpp
@@ -1,10 +1,12 @@
 #include<iostream>
-
+#include<thread> // para lidar com o tempo de execuçao
+#include<chrono> // para definir unidade de tempo (ms, segundos)
 
 int main(){
 
 for(int i = 10; i >= 0; i--){
     std::cout<<"A bomba irá explodir em.."<<i<<std::endl;
+std::this_thread::sleep_for(std::chrono::milliseconds(1000));
 }
 
 
