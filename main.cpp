e4fb140bbc106192efdfc0e84bade6f42342f679
RENATO CHASSOT COLOMBO <03917718090@senacrs.edu.br>
2026-04-15 19:34:33 -0300

    bomba irá explodir

--- main.cpp
+++ main.cpp
@@ -3,8 +3,8 @@
 
 int main(){
 
-for(int i = i; i <= 10; i++){
-    std::cout<<"Este é um loop!!"<<std::endl;
+for(int i = 10; i >= 0; i--){
+    std::cout<<"A bomba irá explodir em.."<<i<<std::endl;
 }
 
 
