3bb559bb6d3f55ae35f7b20bdc3a99d15cdcaeff
RENATO CHASSOT COLOMBO <03917718090@senacrs.edu.br>
2026-04-15 20:21:20 -0300

    dias da semana switch

--- main.cpp
+++ main.cpp
@@ -2,18 +2,38 @@
 
 int main(){
 
-  int tabuada = 7;
+    int opcao;
 
-  for(int i = 0; i <= 10; i++){
-    std::cout<<"O resultado de "<<tabuada<<" x "<<i<<" = "<<tabuada * i<<std::endl;
+    std::cout<<"Digite um valor para opção: "<<std::endl;
+    std::cin>>opcao;
 
-  }
+    switch(opcao){
+      case 1:
+          std::cout<<"Domingo!"<<std::endl;
+          break;
+      case 2:
+          std::cout<<"Segunda-feira!"<<std::endl;
+          break;
+      case 3:
+          std::cout<<"Terça-feira!"<<std::endl;
+          break;
+      case 4:
+          std::cout<<"Quarta-feira!"<<std::endl;
+          break;
+      case 5:
+          std::cout<<"Quinta-feira!"<<std::endl;
+          break;
+      case 6:
+          std::cout<<"Sexta-feira!"<<std::endl;
+          break;
+      case 7:
+          std::cout<<"Sábado!"<<std::endl;
+          break;
+      default:
+          std::cout<<"Número inválido!"<<std::endl;
+    }
 
-
-
-
-
-
+  
 
 
   return 0;
