5f994d04f66c404f0072949f591e3c3038e05f3f
RENATO CHASSOT COLOMBO <03917718090@senacrs.edu.br>
2026-04-15 19:19:17 -0300

    inicio de loop

--- main.cpp
+++ main.cpp
@@ -1,29 +1,10 @@
 #include<iostream>
-#include<string>
-#include<iomanip>
+
 
 int main(){
 
-
-std::string nome;
-int idade;
-float altura;
-
-
-std::cout<<"Qual seu nome: "<<std::endl;
-std::cin>>nome;
-std::cout<<"Qual sua idade: "<<std::endl;
-std::cin>>idade;
-std::cout<<"Qual sua altura: "<<std::endl;
-std::cin>>altura;
-
-
-if(idade <= 10){
-  std::cout<<"Você se enquadra no infantil: "<<std::endl;
-}else if(idade <= 18){
-  std::cout<<"Você se enquadra no adolescente: "<<std::endl;
-}else if(idade <= 60){
-  std::cout<<"Você se enquadra no adulto: "<<std::endl;
+for(int i = i; i <= 10; i++){
+    std::cout<<"Este é um loop!!"<<std::endl;
 }
 
 
@@ -31,16 +12,6 @@ if(idade <= 10){
 
 
 
-
-
-
-
-
-
- 
-
-  
-  
 
   return 0;
 
