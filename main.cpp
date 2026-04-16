409fbabd036e03e00ea946199fd1d1b324a10bab
RENATO CHASSOT COLOMBO <03917718090@senacrs.edu.br>
2026-04-15 21:34:03 -0300

    myNumbers

--- main.cpp
+++ main.cpp
@@ -2,13 +2,10 @@
 
 int main(){
 
-    for(int i = 1; i <= 2; ++i){
-      std::cout<<"Externo: "<<i<<std::endl;
-
-      for(int j = 1; j <= 3; ++j){
-          std::cout<<"Interno: "<<j<<std::endl;
-      }
-    }
+   int myNumbers[5] = {10, 20, 30, 40, 50};
+   for(int num : myNumbers){
+      std::cout<<num<<std::endl;
+   }
 
   return 0;
 
