bcabd8e2e1cfbfc27093067d9c5eee079e1e7f6a
RENATO CHASSOT COLOMBO <03917718090@senacrs.edu.br>
2026-04-20 19:41:00 -0300

    nomes e idades 2.0

--- main.cpp
+++ main.cpp
@@ -7,7 +7,8 @@ std::string nomes[3] = {"João", "Paula", "Murilo"};
 int idades[3] = {27, 31, 20};
 
 std::cout<<nomes[1]<<" tem "<<idades[1]<<" anos! "<<std::endl;
-
+std::cout<<nomes[2]<<" tem "<<idades[2]<<" anos! "<<std::endl;
+std::cout<<nomes[3]<<" tem "<<idades[3]<<" anos! "<<std::endl;
 
 
    return 0;
