014427c5362da5762ffa06b7b25630374d958c1d
RENATO CHASSOT COLOMBO <03917718090@senacrs.edu.br>
2026-04-20 20:13:28 -0300

    carros sem i++

--- main.cpp
+++ main.cpp
@@ -5,11 +5,9 @@ int main(){
       
 std::string carros[5] = {"Volvo", "BMW", "Ford", "Chevrolet", "Fiat"};
 
-for(int i = 0; i < 5; i++){
-    std::cout<<carros[i]<<std::endl;
-}
+int tamanho = sizeof(carros) / sizeof(carros[0]);
 
-
+std::cout<<tamanho<<std::endl;
 
 
 
