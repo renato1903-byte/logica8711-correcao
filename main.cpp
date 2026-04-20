48961d8a756450ecf3fc07bdc5d54bcca7a24eca
RENATO CHASSOT COLOMBO <03917718090@senacrs.edu.br>
2026-04-20 19:36:45 -0300

    nomes e idades

--- main.cpp
+++ main.cpp
@@ -1,30 +1,13 @@
 #include<iostream>
-#include<cstdlib>
-#include<ctime>
+#include<string>
 
 int main(){
-    int jogador, pc;
+      
+std::string nomes[3] = {"João", "Paula", "Murilo"};
+int idades[3] = {27, 31, 20};
 
-    std::srand(std::time(0));
-    pc = std::rand() % 3 + 1;
+std::cout<<nomes[1]<<" tem "<<idades[1]<<" anos! "<<std::endl;
 
-    std::cout<<"1-Pedra 2-Papel 3-Tesoura: ";
-    std::cin>>jogador;
-
-    std::cout<<"PC escolheu: "<<pc<<std::endl;
-
-    if(jogador == pc){
-      std::cout<<"Empate!";
-    }else if(
-        (jogador == 1 && pc == 2) ||
-        (jogador == 2 && pc == 1) ||
-        (jogador == 3 && pc == 2)
-
-    ) {
-          std::cout<<"Você venceu!";
-    }else{
-          std::cout<<"Você perdeu!";
-    }
 
 
    return 0;
