149374c8d89f1209d14a6e41ec90937da460f1d9
RENATO CHASSOT COLOMBO <03917718090@senacrs.edu.br>
2026-04-28 20:13:39 -0300

    myFunction

--- main.cpp
+++ main.cpp
@@ -1,20 +1,14 @@
 #include<iostream>
 
+void myFunction(){
+    std::cout<<"Olá mundo!"<<std::endl;
+}
+
+
 int main(){
       
-    int myNumbers[5] = {1, 2, 3, 4, 5};
+    myFunction();
 
-  // for(int i = 0; i < 5; i++){
-  // std::cout<<mynumbers[i]<<std::endl
-  // }
-
-
-
-
-
-    for(int i = 0; i < 5; i++){
-        std::cout<<myNumbers[i]<<std::endl;
-    }
 
    return 0;
 
