3f7deaf81f9bca1d34f8be74b53643793cb45c40
RENATO CHASSOT COLOMBO <03917718090@senacrs.edu.br>
2026-04-28 21:12:42 -0300

    meu nome

--- main.cpp
+++ main.cpp
@@ -1,15 +1,15 @@
 #include<iostream>
 #include<string>
 
-void minhaFuncao(std::string nome){
-    std::cout<<nome<<std::endl;
+void minhaFuncao(std::string nome, int idade){
+    std::cout<<"Meu nome é "<<nome<<" e tenho "<<idade<<" anos."<<std::endl;
 }
 
 
 int main(){
       
-    minhaFuncao("Renato");
-    minhaFuncao("Nome");
+    minhaFuncao("Renato", 38);
+    
 
    return 0;
 
