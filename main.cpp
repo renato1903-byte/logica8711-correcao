9be795a2d73d8f0695d6c53cf2c86de94ef31fc6
RENATO CHASSOT COLOMBO <03917718090@senacrs.edu.br>
2026-04-30 19:23:51 -0300

    cadastro

--- main.cpp
+++ main.cpp
@@ -1,16 +1,28 @@
 #include<iostream>
-
-
-int doubleGame(int x){
-    return x * 2;
-}
+#include<string>
 
 int main(){
       
-    for(int i = 1; i <= 5; i++){
-      std::cout<<"O dobro de "<<i<<" é "<<doubleGame(i)<<std::endl;
+    int opcao;
+    std::string nome;
+    int idade;
+
+    std::cout<<"----- Escolha a opção adequada: "<<std::endl;
+    std::cout<<"Digite 1 para cadastrar ou 2 para sair"<<std::endl;
+    std::cin>>opcao;
+
+    if(opcao == 1){
+      std::cout<<"Digite o nome: "<<std::endl;
+      std::cin>>nome;
+      std::cout<<"Digite a idade: "<<std::endl;
+      std::cin>>idade;
+
+        std::cout<<"Seu nome é: "<<nome<<std::endl;
+        std::cout<<"Sua idade é: "<<idade<<std::endl;
+
+    }else{
+          std::cout<<"Até a próxima!"<<std::endl;
     }
-    
 
    return 0;
 
