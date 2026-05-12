54f04c6f5980554431fb051d0ec20934bedc31fe
RENATO CHASSOT COLOMBO <03917718090@senacrs.edu.br>
2026-05-12 19:58:58 -0300

    calculadora muito mais elaborada

--- main.cpp
+++ main.cpp
@@ -1,45 +1,38 @@
 #include<iostream>
 
-int main(){
-
-    int num1, num2, soma, subt, mult;
-    float div;
-    int op;
-
-    std::cout<<"Digite aqui o valor para numero 1: "<<std::endl;
-    std::cin>>num1;
-
-    std::cout<<"Digite aqui o valor para numero 2: "<<std::endl;
-    std::cin>>num2;
-
-        std::cout<<"----- Escolha a oprção -----"<<std::endl;
-        std::cout<<"1 - Soma";
-        std::cout<<"2 - Subtração";
-        std::cout<<"3 - Multiplicaçaõ";
-        std::cout<<"4 - divisão";
-
-        switch(op){
-            case 1:
-            soma = num1 + num2;
-            std::cout<<"O resultado da soma é: "<<soma<<std::endl;
-            break;
-            case 2:
-            subt = num1 - num2;
-            std::cout<<"o resulta da subtração é: "<<subt<<std::endl;
-            break;
-            case 3:
-            mult = num1 * num2;
-            std::cout<<"O resultado da divisão é: "<<std::endl;
-            break;
-            case 4:
-            div = num1 / num2;
-            std::cout<<"O resultado da divisão é: "<<std::endl;
-            break;
-            default:
-            std::cout<<"Você não digitou nenhum valor válido!"<<std::endl;
-
-
-        }
-
-    return 0;
+float somar(float a, float b){
+    return a + b;
 }
+
+float subtrair(float a, float b){
+    return a - b;
+}
+
+float multiplicar(float a, float b){
+    return a * b;
+}
+
+float dividir(float a, float b){
+    return a / b;
+}
+
+void coletarNumeros(float &a, float &b){
+    std::cout<<"Digite um valor para a: "<<std::endl;
+    std::cin>>a;
+
+    std::cout<<"Digite um valor para b: "<<std::endl;
+    std::cin>>b;
+}
+
+void exibirResultado(float &a, float&b){
+    coletarNumeros(a, b);
+
+    int op;
+    std::cout<<"Digite uma opcao: "<<std::endl;
+    std::cout<<"1 - Somar"<<std::endl;
+    std::cout<<"2 - Subtrair"<<std::endl;
+    std::cout<<"3 - Multiplicar"<<std::endl;
+    std::cout<<"4 - Dividir"<<std::endl;
+    std::cin>>op;
+}
+
