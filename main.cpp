430157b6e164fe5404745dc7d9a79e4cc5e9bfd5
RENATO CHASSOT COLOMBO <03917718090@senacrs.edu.br>
2026-05-12 19:26:28 -0300

    calculadora facil

--- main.cpp
+++ main.cpp
@@ -1,15 +1,1 @@
 #include<iostream>
-#include<string>
-
-int main(){
-    
-    std::string nomeCompleto;
-
-    std::cout<<"Digite o nome completo do ano: "<<std::endl;
-    std::getline(std::cin, nomeCompleto);
-
-    std::cout<<nomeCompleto<<std::endl;
-
-   return 0;
-
-}
