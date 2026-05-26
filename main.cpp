24c06c0d3573004fe294fcd0426afa2d911694c5
RENATO CHASSOT COLOMBO <03917718090@senacrs.edu.br>
2026-05-26 19:36:21 -0300

    format

--- main.cpp
+++ main.cpp
@@ -1,19 +1,11 @@
 #include<iostream>
+#include<format>
 
 int main(){
-    int numero;
-    int contador = 0;
+    std::string nome = "Renato";
 
-    while(true){
-        std::cout<<"Digite um número (0 para parar): ";
-        std::cin>>numero;
-
-        if(numero == 0){
-            break;
-        }
-            contador++;
-    }
-       std::cout<<"Você digitou "<<contador<<" números!"<<std::endl;
+    std::string mensagem = std::format("Olá, {}! Você tem {} anos.\n", nome);
+    std::cout<<mensagem;
 
     return 0;
         
