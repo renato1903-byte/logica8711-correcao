3972f1235546f51b728cf50fe5bf95abb1a3ec64
RENATO CHASSOT COLOMBO <03917718090@senacrs.edu.br>
2026-05-28 19:56:35 -0300

    recursao com fibonacci

--- main.cpp
+++ main.cpp
@@ -1,29 +1,16 @@
 #include<iostream>
-#include<tuple>
-#include<string>
+
+int fibonacci(int n){
+    if(n == 0) return 0;
+    if(n == 1) return 1;
+
+    return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
 
 int main(){
+     std::cout<<fibonacci(6)<<std::endl; 
     
-    std::string nome;
-    int idade;
-    double altura;
-
-    std::cout<<"Digite o seu nome: "<<std::endl;
-    std::getline(std::cin, nome);
-
-    std::cout<<"Digite a sua idade: "<<std::endl;
-    std::cin>>idade;
-
-    std::cout<<"Digite a sua altura"<<std::endl;
-    std::cin>>altura;
-
-    std::tuple<std::string, int, double> pessoa = std::make_tuple(nome, idade, altura);
-
-    std::cout<<"\n ----- Dados salvos na Tupla -----"<<std::endl;
-
-    std::cout<<"Nome: "<<std::get<0>(pessoa)<<std::endl;
-    std::cout<<"Idade: "<<std::get<1>(pessoa)<<" anos."<<std::endl;
-    std::cout<<"Altura: "<<std::get<2>(pessoa)<<"m"<<std::endl;
 
     return 0;
 }
