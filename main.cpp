3858faaede50678fb7aa06e7f01326f85768e11b
RENATO CHASSOT COLOMBO <03917718090@senacrs.edu.br>
2026-06-02 20:24:29 -0300

    numero, endereco e idade

--- main.cpp
+++ main.cpp
@@ -1,36 +1,15 @@
 #include <iostream>
-#include <string>
-#include <cctype>
-
-
-bool ehPalindromo(std::string s, int inicio, int fim){
-     if(inicio >= fim){
-          return true;
-     }
-     if(s[inicio] != s[fim]){
-          return false;
-     }
-     return ehPalindromo(s, inicio + 1, fim -1);
-}
-
-
 
 
 int main(){
 
-     std::string palavra;
-     std::cout<<"Digite uma palavra: "<<std::endl;
-     std::cin>>palavra;
+     int numero = 1;
 
-     for(char &c : palavra){
-          c = std::tolower(c);
-     }
-     if(ehPalindromo(palavra, 0, palavra.length() - 1)){
-          std::cout<<"Eh um palindromo!"<<std::endl;
+     std::cout<<"numero: "<<numero<<std::endl;
+     std::cout<<"Endereço: "<<&numero<<std::endl;
 
-     }else{
-          std::cout<<"Não é um palindromo!"<<std::endl;
-     }
+     
+     
      
      return 0;
 }
