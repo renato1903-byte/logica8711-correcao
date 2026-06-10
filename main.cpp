446180910ae5f3d8efe69edf9158f8e1a57339fb
RENATO CHASSOT COLOMBO <03917718090@senacrs.edu.br>
2026-06-10 19:08:43 -0300

    inicio de orientacao a objetos

--- main.cpp
+++ main.cpp
@@ -1,9 +1,1 @@
-#include <iostream>
-
-int main(){
-    int idade = 38;
-
-    std::string resultado = (idade >= 18) ? "Maior idade!" : "Menor de idade!";
-
-    std::cout<<resultado<<std::endl;
-}
+// inicio de phyton
