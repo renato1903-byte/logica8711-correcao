d40a6eb0378508f8e028fc5eb51a01fe1365595e
RENATO CHASSOT COLOMBO <03917718090@senacrs.edu.br>
2026-06-11 19:53:18 -0300

    quickSort

--- main.cpp
+++ main.cpp
@@ -1,9 +1,38 @@
-#include<iostream>
+#include <iostream>
+#include <string>
 
-int main(){
-    int idade = 38;
+void quickSort(int *arr, int esquerda, int direita)
+{
+    if (esquerda >= direita)
+        return;
 
-    std::string resultado = (idade >= 18) ? "Maior de idade!" : "Menor de idade!";
+    int pivo = arr[direita];
+    int i = esquerda - 1;
 
-    std::cout<<resultado<<std::endl;
+    for (int j = esquerda; j < direita; j++)
+    {
+        if (arr[j] < pivo)
+        {
+            i++;
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+    }
+    int temp = arr[i + 1];
+    arr[i + 1] = arr[direita];
+    arr[direita] = temp;
+
+    quickSort(arr, esquerda, i);
+    quickSort(arr, i + 2, direita);
+}
+int main()
+{
+    int arr[] = {64, 34, 25, 12, 22, 11, 90};
+    int tamanho = 7;
+
+    for (int i = 0; i < tamanho; i++)
+    {
+        std::cout << arr[i] << " ";
+    }
 }
