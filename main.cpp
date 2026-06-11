22720e77d86d36f60a0baf5391399dd4563c3d32
RENATO CHASSOT COLOMBO <03917718090@senacrs.edu.br>
2026-06-11 20:22:56 -0300

    mdc

--- main.cpp
+++ main.cpp
@@ -1,38 +1,14 @@
 #include <iostream>
 #include <string>
 
-void quickSort(int *arr, int esquerda, int direita)
-{
-    if (esquerda >= direita)
-        return;
-
-    int pivo = arr[direita];
-    int i = esquerda - 1;
-
-    for (int j = esquerda; j < direita; j++)
-    {
-        if (arr[j] < pivo)
-        {
-            i++;
-            int temp = arr[i];
-            arr[i] = arr[j];
-            arr[j] = temp;
-        }
-    }
-    int temp = arr[i + 1];
-    arr[i + 1] = arr[direita];
-    arr[direita] = temp;
-
-    quickSort(arr, esquerda, i);
-    quickSort(arr, i + 2, direita);
+int mdc(int a, int b){
+    if(b == 0) return a;
+    return mdc(b, a %b);
 }
-int main()
-{
-    int arr[] = {64, 34, 25, 12, 22, 11, 90};
-    int tamanho = 7;
 
-    for (int i = 0; i < tamanho; i++)
-    {
-        std::cout << arr[i] << " ";
-    }
+int main(){
+    std::cout<<"MDC(48, 18) = "<<mdc(48, 18)<<std::endl;
+    std::cout<<"MDC(100, 50) = "<<mdc(100, 50)<<std::endl;
+
+    return 0;
 }
