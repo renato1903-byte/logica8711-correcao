199c79e1d2dd154e4e66ce29f1c779a98c5bc393
RENATO CHASSOT COLOMBO <03917718090@senacrs.edu.br>
2026-06-17 19:54:36 -0300

    primeira aula de python

--- main.cpp
+++ main.cpp
@@ -1,43 +1,34 @@
 #include <iostream>
 #include <string>
 
-struct jogador{
-    std::string nome;
+struct Aposta{
     std::string time;
-    int gols;
+    int valor;
+    int odd;
 };
 
 int main(){
-
-    jogador jogadores[5];
-
-    std::cout<<"===== COPA - RANKING DE GOLEADORES ====="<<std::endl;
+    std::cout<<"===== CALCULADORE DE APOSTAS ====="<<std::endl;
     std::cout<<std::endl;
 
-    for(int i = 0; i < 5; i++){
-        std::cout<<"Jogador: "<<(i + 1)<<":"<<std::endl;
-        std::cout<<"Nome: ";
-        std::cin>>jogadores[i].nome;
-        std::cout<<"Time: ";
-        std::cin>>jogadores[i].time;
-        std::cout<<"Gols: ";
-        std::cin>>jogadores[i].gols;
-        std::cout<<std::endl;
-    }
-    std::cout<<"===== RANKING ====="<<std::endl;
-    std::cout<<std::endl;
+    Aposta aposta;
 
-    for(int i = 0; i < 5; i++){
-       for(int j = 0; j < 5 - 1; j++){
-        if(jogadores[i].gols < jogadores[j].gols){
-            jogador temp = jogadores[j];
-            jogadores[j] = jogadores[j + 1];
-            jogadores[j + 1] = temp;
-         }
-       }
-    }
-    for(int i = 0; i < 5; i++){
-        std::cout<<(i + 1)<<"º - "<<jogadores[i].nome<<" ("<<jogadores[i].time<<") - "<<jogadores[i].gols<<" gols"<<std::endl;
-    } 
-    return 0;
+    std::cout<<"Time: ";
+    std::cin>>aposta.time;
+
+    std::cout<<"Valor da aposta (R$): ";
+    std::cin>>aposta.valor;
+
+    std::cout<<"Odd (multiplicador): ";
+    std::cin>>aposta.odd;
+
+    int ganho = aposta.valor * aposta.odd;
+    int lucro = ganho - aposta.valor;
+
+    std::cout<<std::endl;
+    std::cout<<"===== RESULTADO ====="<<std::endl;
+    std::cout<<"Você apostou R$"<<aposta.valor<<" em "<<aposta.time<<std::endl;
+    std::cout<<"Se acertar, você ganha: R$ "<<ganho<<std::endl;
+    std::cout<<"Lucro: R$ "<<lucro<<std::endl;
+
 }
