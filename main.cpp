2d61b65ca4236b8945db75edeefa1df941890ac5
RENATO CHASSOT COLOMBO <03917718090@senacrs.edu.br>
2026-06-23 20:12:38 -0300

    array

--- main.cpp
+++ main.cpp
@@ -2,92 +2,29 @@
 
 int main(){
 
-    int fila[10];
-    int inicio = 0;
-    int fim = 0;
-    int opcao;
+    int numeros[] = {1, 2, 3, 4, 5, 3, 6};
+    int tamanho = 8;
 
-    std::cout<<"===== FILA COM MENU ====="<<std::endl;
+    std::cout<<"===== NÚMERO DUPLICADOS ====="<<std::endl;
+    std::cout<<std::endl;
 
-    while(true){
+    std::cout<<"Array: ";
+    for(int i = 0; i < tamanho; i++){
+        std::cout<<numeros[i]<<" ";
+    } 
+    std::cout<<std::endl;
 
-        std::cout<<"\n1. Enfileirar"<<std::endl;
-        std::cout<<"2. Desenfileirar"<<std::endl;
-        std::cout<<"3. Exibir fila"<<std::endl;
-        std::cout<<"4. Sair"<<std::endl;
-        std::cout<<"Escolha: ";
-        std::cin>>opcao;
-
-        if(opcao == 1){
-
-            if(fim == 10){
-                std::cout<<"Fila cheia!"<<std::endl;
-            }else{
-                int valor;
-                std::cout<<"Digite um valor: ";
-                std::cin>>valor;
-
-                fila[fim] = valor;
-                fim++;
-
-                std::cout<<"Valor enfileirado!"<<std::endl;
+    std::cout<<"Números duplicados: "<<std::endl;
+    for(int i = 0; i < tamanho; i++){
+        for(int j = i + 1; j < tamanho; j++){
+            if(numeros[i] == numeros[j]){
+                std::cout<<numeros[i]<<" aparece mais de uma vez!"<<std::endl;
+                break;
             }
-
-        }else if(opcao == 2){
-
-            if(inicio == fim){
-                std::cout<<"Fila vazia!"<<std::endl;
-            }else{
-                std::cout<<"Removido: "<<fila[inicio]<<std::endl;
-                inicio++;
-            }
-
-        }else if(opcao == 3){
-
-            if(inicio == fim){
-                std::cout<<"Fila vazia!"<<std::endl;
-            }else{
-                std::cout<<"Fila: ";
-                for(int i = inicio; i < fim; i++){
-                    std::cout<<fila[i]<<" ";
-                }
-                std::cout<<std::endl;
-            }
-
-        }else if(opcao == 4){
-
-            std::cout<<"Encerrando..."<<std::endl;
-            break;
-
-        }else{
-
-            std::cout<<"Opcao invalida!"<<std::endl;
-
         }
     }
-
     return 0;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
