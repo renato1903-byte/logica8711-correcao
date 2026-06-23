f5c510b59f1efcb7fd42f7ad927ea1923701f297
RENATO CHASSOT COLOMBO <03917718090@senacrs.edu.br>
2026-06-23 19:31:19 -0300

    pilha 2.0

--- main.cpp
+++ main.cpp
@@ -1,25 +1,69 @@
 #include <iostream>
 
 int main(){
-    int fila[5];
-    int inicio = 0;
-        int fim = 0;
 
-    std::cout<<"===== FILA ====="<<std::endl;
-    std::cout<<std::endl;
+    int pilha[10];
+    int topo = -1;
+    int opcao;
+    int valor;
 
-    fila[0] = 100;
-    fila[1] = 200;
-    fila[2] = 300;
-    fim = 3;
+    std::cout<<"===== PILHA COM MENU ====="<<std::endl;
 
-    std::cout<<"Elementos enfileirados: 100, 200, 300"<<std::endl;
-    std::cout<<std::endl;
-    std::cout<<"Desenfileirando"<<std::endl;
-    while(inicio < fim){
-        std::cout<<"Removido: "<<fila[inicio]<<std::endl;
-        inicio++;
+    while(true){
+
+        std::cout<<"\n1. Empilhar"<<std::endl;
+        std::cout<<"2. Desempilhar"<<std::endl;
+        std::cout<<"3. Exibir pilha"<<std::endl;
+        std::cout<<"4. Sair"<<std::endl;
+        std::cout<<"Escolha: ";
+        std::cin>>opcao;
+
+        if(opcao == 1){
+
+            if(topo == 9){
+                std::cout<<"Pilha cheia!"<<std::endl;
+            }else{
+                std::cout<<"Digite um valor: ";
+                std::cin>>valor;
+
+                topo++;
+                pilha[topo] = valor;
+
+                std::cout<<"Valor empilhado!"<<std::endl;
+            }
+
+        }else if(opcao == 2){
+
+            if(topo == -1){
+                std::cout<<"Pilha vazia!"<<std::endl;
+            }else{
+                std::cout<<"Valor removido: "<<pilha[topo]<<std::endl;
+                topo--;
+            }
+
+        }else if(opcao == 3){
+
+            if(topo == -1){
+                std::cout<<"Pilha vazia!"<<std::endl;
+            }else{
+                std::cout<<"Elementos da pilha:"<<std::endl;
+
+                for(int i = topo; i >= 0; i--){
+                    std::cout<<pilha[i]<<std::endl;
+                }
+            }
+
+        }else if(opcao == 4){
+
+            std::cout<<"Encerrando programa..."<<std::endl;
+            break;
+
+        }else{
+
+            std::cout<<"Opcao invalida!"<<std::endl;
+
+        }
     }
-    
+
     return 0;
 }
