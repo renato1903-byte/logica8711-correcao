4969d3db4bf10c1229314e569fe900422d31a58c
RENATO CHASSOT COLOMBO <03917718090@senacrs.edu.br>
2026-06-23 20:50:32 -0300

    arrayy 2.0

--- main.cpp
+++ main.cpp
@@ -2,27 +2,25 @@
 
 int main(){
 
-    int numeros[] = {1, 2, 3, 4, 5, 3, 6};
-    int tamanho = 8;
+    int numeros[] = {1,2,3,4,5,6,7,8,9,10};
+    int tamanho;
 
-    std::cout<<"===== NÚMERO DUPLICADOS ====="<<std::endl;
+    std::cout<<"===== PARES E ÍMPARES ====="<<std::endl;
     std::cout<<std::endl;
 
-    std::cout<<"Array: ";
+    std::cout<<"Pares: ";
     for(int i = 0; i < tamanho; i++){
-        std::cout<<numeros[i]<<" ";
-    } 
-    std::cout<<std::endl;
-
-    std::cout<<"Números duplicados: "<<std::endl;
-    for(int i = 0; i < tamanho; i++){
-        for(int j = i + 1; j < tamanho; j++){
-            if(numeros[i] == numeros[j]){
-                std::cout<<numeros[i]<<" aparece mais de uma vez!"<<std::endl;
-                break;
-            }
+        if(numeros[i] % 2 == 0){
+            std::cout<<" ";
         }
     }
+    std::cout<<std::endl;
+    std::cout<<"Ímpares: ";
+    for(int i = 0; i < tamanho; i++){
+        if(numeros[i] % 2 != 0){
+            std::cout<<numeros[i]<<" ";
+        }
+    } 
     return 0;
 }
 
