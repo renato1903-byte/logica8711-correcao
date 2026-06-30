a2f7b4d569a32ed29f1404e48f556b190275be82
RENATO CHASSOT COLOMBO <03917718090@senacrs.edu.br>
2026-06-30 19:48:47 -0300

    atividade03

--- main.cpp
+++ main.cpp
@@ -1,16 +1,23 @@
 #include <iostream>
 
 int main(){
-    
-   int numeros[5];
-   int soma = 0;
 
-   for(int i = 0; i < 5; i++){
-    std::cout<<"Digite o"<<i + 1<<"numero: "<<std::endl;
-    std::cin>>numeros[i];
-    soma += numeros[i];
-   }
-   std::cout<<"\n Soma total: "<<soma<<std::endl;
-    
+    int numeros[5];
+
+    for(int i = 0; i < 5; i++){
+        std::cout<<"Digite o "<<i + 1<<" numero: ";
+        std::cin>>numeros[i];
+    }
+
+    int maior = numeros[0];
+
+    for(int i = 1; i < 5; i++){
+        if(numeros[i] > maior){
+            maior = numeros[i];
+        }
+    }
+
+    std::cout<<"Maior numero: "<<maior<<std::endl;
+
     return 0;
 }
