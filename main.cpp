b7c65d1fce4502f3f1e7833a178139a89b390597
RENATO CHASSOT COLOMBO <03917718090@senacrs.edu.br>
2026-06-30 20:53:47 -0300

    atividade07

--- main.cpp
+++ main.cpp
@@ -2,21 +2,14 @@
 
 int main() {
 
-    int numeros[5];
-    int pares = 0;
+    int numeros[10];
 
-    for (int i = 0; i < 5; i++) {
+    for(int i = 0; i < 10; i++){
         std::cout << "Digite o " << i + 1 << " numero: ";
         std::cin >> numeros[i];
     }
 
-    for (int i = 0; i < 5; i++) {
-        if (numeros[i] % 2 == 0) {
-            pares++;
-        }
-    }
-
-    std::cout << "\nQuantidade de numeros pares: " << pares << std::endl;
+    std::cout << "\nO array possui " << 10 << " elementos." << std::endl;
 
     return 0;
 }
