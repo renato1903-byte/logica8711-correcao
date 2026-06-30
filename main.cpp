9321dc79a5729c7f4e708ed96622ff4bde203740
RENATO CHASSOT COLOMBO <03917718090@senacrs.edu.br>
2026-06-30 20:58:59 -0300

    atividade08

--- main.cpp
+++ main.cpp
@@ -9,7 +9,15 @@ int main() {
         std::cin >> numeros[i];
     }
 
-    std::cout << "\nO array possui " << 10 << " elementos." << std::endl;
+    std::cout << "\nNumeros impares:" << std::endl;
+
+    for(int i = 0; i < 10; i++){
+        if(numeros[i] % 2 != 0){
+            std::cout << numeros[i] << " ";
+        }
+    }
+
+    std::cout << std::endl;
 
     return 0;
 }
