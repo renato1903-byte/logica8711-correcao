8fc1b6433451cd0828107118e5604ab566225ee4
RENATO CHASSOT COLOMBO <03917718090@senacrs.edu.br>
2026-06-30 21:01:47 -0300

    atividade09

--- main.cpp
+++ main.cpp
@@ -2,19 +2,17 @@
 
 int main() {
 
-    int numeros[10];
+    int numeros[5];
 
-    for(int i = 0; i < 10; i++){
+    for(int i = 0; i < 5; i++){
         std::cout << "Digite o " << i + 1 << " numero: ";
         std::cin >> numeros[i];
     }
 
-    std::cout << "\nNumeros impares:" << std::endl;
+    std::cout << "\nNumeros em ordem inversa:" << std::endl;
 
-    for(int i = 0; i < 10; i++){
-        if(numeros[i] % 2 != 0){
-            std::cout << numeros[i] << " ";
-        }
+    for(int i = 4; i >= 0; i--){
+        std::cout << numeros[i] << " ";
     }
 
     std::cout << std::endl;
