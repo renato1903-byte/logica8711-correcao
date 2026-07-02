68233b49b2d9de61e1dc84707c595a5a338cfabc
RENATO CHASSOT COLOMBO <03917718090@senacrs.edu.br>
2026-07-02 20:19:34 -0300

    Arraylist 4.0

--- main.cpp
+++ main.cpp
@@ -5,17 +5,23 @@ int main() {
     
     std::vector<int>numeros;
 
-    std::cout<<"===== 2. Tamanho do vector ====="<<std::endl;
+    std::cout<<"===== Acessar elemento ====="<<std::endl;
 
-    numeros.push_back(5);
-    numeros.push_back(10);
-    numeros.push_back(15);
+    numeros.push_back(100);
+    numeros.push_back(200);
+    numeros.push_back(300);
+    numeros.push_back(400);
 
-    std::cout<<"Tamanho do vector: "<<numeros.size()<<std::endl;
-    std::cout<<"Elementos: ";
+    std::cout<<"Vector: ";
     for(int i = 0; i < numeros.size(); i++){
         std::cout<<numeros[i]<<" ";
+
     }
+    std::cout<<std::endl;
+
+    std::cout<<"Elemento na posição 0: "<<numeros[0]<<std::endl;
+    std::cout<<"Elemento na posição 2: "<<numeros[2]<<std::endl;
+    std::cout<<"Último elemento: "<<numeros[numeros.size() - 1]<<std::endl;
 
     return 0;
 }
