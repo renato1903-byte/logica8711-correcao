79f74ae4b398689d468385dc266483ab448437e2
RENATO CHASSOT COLOMBO <03917718090@senacrs.edu.br>
2026-07-02 20:05:19 -0300

    Arraylist 3.0

--- main.cpp
+++ main.cpp
@@ -5,30 +5,17 @@ int main() {
     
     std::vector<int>numeros;
 
-    std::cout<<"===== VECTOR (Arraylist em C++)"<<std::endl;
+    std::cout<<"===== 2. Tamanho do vector ====="<<std::endl;
 
+    numeros.push_back(5);
     numeros.push_back(10);
-    numeros.push_back(20);
-    numeros.push_back(30);
+    numeros.push_back(15);
 
-    std::cout<<"Numeros: ";
+    std::cout<<"Tamanho do vector: "<<numeros.size()<<std::endl;
+    std::cout<<"Elementos: ";
     for(int i = 0; i < numeros.size(); i++){
         std::cout<<numeros[i]<<" ";
     }
-    std::cout<<std::endl;
 
-    std::cout<<"Tamanho: "<<numeros.size()<<std::endl;
-
-    int posicao = 2;
-    std::cout<<"Digite uma posição: "<<std::endl;
-    numeros.erase(numeros.begin() + posicao);
-
-
-
-
-    std::cout<<"Após remover o último: ";
-    for(int i = 0; i < numeros.size(); i++){
-        std::cout<<numeros[i]<<" ";
-    }
     return 0;
 }
