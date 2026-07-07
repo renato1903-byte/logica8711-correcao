2f853be41eef828816b12d963b2febee59a15da7
RENATO CHASSOT COLOMBO <03917718090@senacrs.edu.br>
2026-07-07 19:44:37 -0300

    att 3

--- main.cpp
+++ main.cpp
@@ -4,30 +4,34 @@
 int main() {
     
     std::vector<int>numeros;
-    std::cout<<"Contar ocorrências: "<<std::endl;
-    numeros.push_back(5);
-    numeros.push_back(10);
-    numeros.push_back(5);
-    numeros.push_back(15);
-    numeros.push_back(15);
-    numeros.push_back(5);
-    numeros.push_back(15);
+    std::cout<<"Buscar elementos: "<<std::endl;
+    numeros.push_back(100);
+    numeros.push_back(200);
+    numeros.push_back(300);
+    numeros.push_back(400);
+   
 
    
 
     
     std::cout<<"Vector = ";
-    for(int i = 0; i <numeros.size(); i++ ){
+    for(int i = 0; i < numeros.size(); i++ ){
         std::cout<<numeros[i]<<" ";
     }
-    int contador = 0;
+    int buscado = 0;
+    int posicao = 1;
     for(int i = 0; i <numeros.size(); i++ ){
-        if(numeros[i] == 5){
-            contador++;
+        if(numeros[i] == buscado){
+            posicao = i;
+            break;
         }   
 }
     
-    std::cout<<"O número 5 aparece: "<<contador<<std::endl;
+   if(posicao != -1){
+    std::cout<<"Número "<<buscado<<" encontrado na posicao "<<posicao<<std::endl;
+   }else{
+    std::cout<<"Número não encontrado!"<<std::endl;
+   }
     return 0;
 
 }
