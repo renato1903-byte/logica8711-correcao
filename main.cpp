d753b8e0dbd985bf1ef247ff37aa9c10ad213ee6
RENATO CHASSOT COLOMBO <03917718090@senacrs.edu.br>
2026-07-07 21:27:27 -0300

    att 5

--- main.cpp
+++ main.cpp
@@ -3,23 +3,33 @@
 
 int main() {
     
-    std::vector<int>numeros;
-    int quantidade;
-    std::cout<<"Adicionar do usuário"<<std::endl;
-
-    std::cout<<"Quantos números?"<<std::endl;
-    std::cin>>quantidade;
+    std::vector<int>numeros;                
     
-    for(int i = 0; i < quantidade; i++ ){
-        int numero;
-        std::cout<<"Digite o número "<<(i + 1)<<": ";
-        std::cin>>numero;
-        numeros.push_back(numero);
-    }
-    std::cout<<"Números adicionados: ";
+    numeros.push_back(5);
+    numeros.push_back(10);
+    numeros.push_back(15);
+    numeros.push_back(20);
+    numeros.push_back(25);
+    numeros.push_back(30);
+    // Apresenta todos os números
+    std::cout<<"Vector: ";
     for(int i = 0; i < numeros.size(); i++){
         std::cout<<numeros[i]<<" ";
     }
+   // Mostra apenas os pares
+   std::cout<<"\nPares: ";
+   for(int i = 0; i < numeros.size(); i++){
+        if(numeros[i] % 2 == 0){
+            std::cout<<numeros[i]<<" ";
+        }
+   }
+   // Mostra todos os pares
+   std::cout<<"\nImpares: ";
+   for(int i = 0; i < numeros.size(); i++){
+    if(numeros[i] % 2 != 0 ){
+        std::cout<<numeros[i]<<" ";
+    }
+   }
     return 0;
 
 }
