db388aeb9170a54c15903675136008bb9174a453
RENATO CHASSOT COLOMBO <03917718090@senacrs.edu.br>
2026-07-09 19:35:30 -0300

    ativade 1 sor edu

--- main.cpp
+++ main.cpp
@@ -1,28 +1,37 @@
 #include <iostream>
-#include <vector>
+#include <string>
+
+using namespace std;
 
 int main() {
-    
-    std::vector<int>numeros;
+   string palavra;
+  
 
-    std::cout<<"Inverter o vector: "<<std::endl;
+   std::cout<<"Digite uma palavra: "<<std::endl;
+   std::cin>>palavra;
 
-    numeros.push_back(5);
-    numeros.push_back(10);
-    numeros.push_back(15);
-    numeros.push_back(20);
-    
-    std::cout<<"Original: ";
-    for(int i  = 0; i < numeros.size(); i++){
-        std::cout<<numeros[i]<<" ";
-    }
-    std::cout<<std::endl;
-    std::cout<<"Invertido: ";
-    for(int i = numeros.size() -1; i >= 0; i--){
-        std::cout<<numeros[i]<<" ";
 
-    }
-    std::cout<<std::endl;
+   std::cout<<"A palavra digitada foi: "<<palavra<<std::endl;
+   std::cout<<"A primeira letra é: "<<palavra[0]<<std::endl; 
+   
+
+   
     return 0;
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// quero que o usuario coloque uma palavra, e essa palavra seja apresentada com um cout.. 
+// tambem quero que mostre qual foia primeira letra da palavra, usando apenas uma variavel
