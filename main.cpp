a6b6d8cfa8a8da94b8d0c9d86b7bb7e38baa8448
RENATO CHASSOT COLOMBO <03917718090@senacrs.edu.br>
2026-07-16 19:36:45 -0300

    ATIVIDADE 12

--- main.cpp
+++ main.cpp
@@ -1,16 +1,17 @@
 #include <iostream>
 
+using namespace std;
+
 int main() {
-    int numero;
+    int contador = 0;
 
-    std::cout<<"Digite um numero para ver a tabuada: ";
-    std::cin>>numero;
-
-    std::cout<<"\nTabuada do "<<numero<<":\n";
-
-    for(int i = 1; i <= 10; i++){
-        std::cout<<numero<<" x "<<i<<" = "<<numero * i<<"\n";
+    for(int i = 1; i <= 100; i++){
+       if( i % 2 == 0){
+        contador++;
+       }
     }
+    std::cout<<"Existem: "<<contador<<std::endl;
+
     return 0;
 
 }
